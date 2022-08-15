9295cda41c5e57fca091c2fa6985b3105726bf51
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-08-15 12:47:49 +0000

    Navbar Complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ Clients, CTA, Footer } from './components';
 
 
 const App = () =>  (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div style={{backgroundColor: "#000"}} className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
          <Navbar />
@@ -18,7 +18,7 @@ const App = () =>  (
        </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div  className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
        <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
